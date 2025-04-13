e07ffa39a1674a9de29fb7614d4aa3fa4e8b1058
Ravy Bomfim <87732549+RavyBomfim@users.noreply.github.com>
2025-04-13 11:03:02 -0300

    Update menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,20 @@
 window.onload = function() {
 
-    document.querySelector("#menu-mobile").addEventListener("click", function() {
-        if(document.querySelector("#menu ul").style.display == 'flex') {
-            document.querySelector("#menu ul").style.display = 'none'
-        } else {
-            document.querySelector("#menu ul").style.display = 'flex'
+    const menu_button = document.querySelector('#menu-mobile');
+    const menu = document.querySelector("#menu ul");
+
+    menu_button.addEventListener('click', function(event) {
+        menu.classList.toggle('menu');
+
+        event.stopPropagation();
+    });
+
+    document.addEventListener('click', function(event) {
+        const menu_clicked = menu.contains(event.target);
+        const menu_button_clicked = menu_button.contains(event.target);
+
+        if (!menu_clicked && !menu_button_clicked) {
+            menu.classList.remove('menu');
         }
     });
 
